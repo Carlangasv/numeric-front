412b503a48331012e4c68c8232af26d48cc260bb
BryanTorresCalle <50812831+BryanTorresCalle@users.noreply.github.com>
2021-11-11 07:53:17 -0500

    Few changes on index

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="12">
+    <v-col cols="8" sm="6" md="8">
       <v-card>
-        <v-card-title class="headline">
+        <v-card-title class="headline text-h3 font-weight-bold">
           Bienvenido
         </v-card-title>
-        <v-card-text>
+        <v-img src="http://www.cs.buap.mx/~mrodriguez/numerico.png" style="witdh=75px; height=50px"></v-img>
+        <v-card-text style="">
+          <span class="text-h5 font-weight-bold">
+          Elaborado por:
+          <br>
+          Jhon Steven Agudelo Rodriguez
+          <br>
+          Bryan Torres Calle
+          <br>
+          Carlos Andrés Valencia Rada
+          
+          </span>
         </v-card-text>
       </v-card>
     </v-col>
